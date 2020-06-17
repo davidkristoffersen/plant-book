a7fe3c2f8b4e7c22c2a7cab8efc625ae1c310354
David Kristoffersen [Dualog] <david@dualog.no>
2020-06-17 11:09:44 +0200

    grid and img

--- src/app/const-plants.ts
+++ src/app/const-plants.ts
@@ -1,7 +1,36 @@
 import { Plant } from './plant';
 
 export const PLANTS: Plant[] = [
-  { id: 1, name: 'Potnus', size: 1 },
-  { id: 2, name: 'Lykketrollet', size: 4 },
-  { id: 3, name: 'Baby Alfred', size: 3 },
+  {
+    id: 1,
+    name: 'Potnus',
+    size: 1,
+    imgUrl:
+      'https://c8.alamy.com/comp/D8B8P9/plants-growing-in-biodegradable-plant-pots-D8B8P9.jpg',
+    imgText: 'Small boi',
+  },
+  {
+    id: 2,
+    name: 'Lykketrollet',
+    size: 4,
+    imgUrl:
+      'https://c8.alamy.com/comp/X515W6/human-holding-injection-and-the-plant-pot-X515W6.jpg',
+    imgText: 'Thicc boi',
+  },
+  {
+    id: 3,
+    name: 'Baby Alfred',
+    size: 3,
+    imgUrl:
+      'https://c8.alamy.com/comp/X1FD7F/close-up-of-basil-plant-in-pot-X1FD7F.jpg',
+    imgText: 'Baby boi',
+  },
+  {
+    id: 4,
+    name: 'Pilten',
+    size: 2,
+    imgUrl:
+      'https://media.gettyimages.com/photos/potted-plant-against-white-background-picture-id888270500?s=612x612',
+    imgText: 'Sad boi :(',
+  },
 ];
--- src/app/plant.ts
+++ src/app/plant.ts
@@ -1,5 +1,9 @@
+import { Url } from 'url';
+
 export interface Plant {
   id: number;
   name: string;
   size: number;
+  imgUrl: string;
+  imgText: string;
 }
